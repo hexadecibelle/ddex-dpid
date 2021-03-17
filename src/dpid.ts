a01c360b3e56be67de506ce92cefdf961dda805d
Laine Farrell <laine@openimp.com>
2021-03-17 17:01:20 +0000

    Fixing validate so it throws an error if the input length is not 18 characters

--- src/dpid.ts
+++ src/dpid.ts
@@ -4,6 +4,10 @@ const { Mod37_36 } = require( '@konfirm/iso7064' );
 
 const DPid = {
 	validate: function( inp:string ):boolean {
+		if( inp.length !== 18 ) {
+			throw new Error( 'Input is incorrect length. Expects 18 characters.' );
+		}
+
 		return Mod37_36.validate( inp );
 	}
 };
