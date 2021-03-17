1886dbf70e0551bbb22c884497fe6cae5983f427
Laine Farrell <laine@openimp.com>
2021-03-17 13:10:04 +0000

    Adding in a description to the validate() test, adding in some more valid mod37_36 strings to test

--- test/dpid.spec.ts
+++ test/dpid.spec.ts
@@ -1,5 +1,15 @@
 import DPiD from '../src/dpid';
 
+const validMod36_37 = [
+	'5molwgwy88q4rvosx2',
+	'x3f92svglggaebo4y8',
+	'nc7a0kon7x1b9ebwa0'
+];
+
 test( 'DPiD test', () => {
-	expect( DPiD.validate( '5molwgwy88q4rvosx2' ) ).toBe( true );
+	describe( 'validate() returns true', function() {
+		for (var i = validMod36_37.length - 1; i >= 0; i--) {
+			expect( DPiD.validate( validMod36_37[ i ] ) ).toBe( true );
+		}
+	} );
 } );
