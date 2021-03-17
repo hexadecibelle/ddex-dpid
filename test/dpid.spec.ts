a2a0b404add6cfecf3e3605e6feaf3e521bd222e
Laine Farrell <laine@openimp.com>
2021-03-17 13:29:12 +0000

    Rewriting the tests with better expectations and descriptions, using the array of valid mod37_37 strings

--- test/dpid.spec.ts
+++ test/dpid.spec.ts
@@ -6,10 +6,11 @@ const validMod36_37 = [
 	'nc7a0kon7x1b9ebwa0'
 ];
 
-test( 'DPiD test', () => {
-	describe( 'validate() returns true', function() {
-		for (var i = validMod36_37.length - 1; i >= 0; i--) {
+
+describe( 'validate() returns true', () => {
+	for( let i:number = validMod36_37.length - 1; i >= 0; i-- ) {
+		it( 'Should return true when input is ' + validMod36_37[ i ], () => {
 			expect( DPiD.validate( validMod36_37[ i ] ) ).toBe( true );
-		}
-	} );
+		} );
+	}
 } );
