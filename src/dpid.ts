c30f01473979e6cb0e3b3e886161edfb711166ec
Laine Farrell <laine@openimp.com>
2021-03-17 16:25:10 +0000

    Changing the checksum lib dependency back to @konfirm/iso7064
    Regenerated the mod37_36 checksums correctly this time

--- src/dpid.ts
+++ src/dpid.ts
@@ -1,9 +1,11 @@
-import { mod37_36 } from 'cdigit';
+// import { mod37_36 } from 'cdigit';
 
-const DPiD = {
+const { Mod37_36 } = require( '@konfirm/iso7064' );
+
+const DPid = {
 	validate: function( inp:string ):boolean {
 		return true;
 	}
 };
 
-export default DPiD;
+export default DPid;
--- test/dpid.spec.ts
+++ test/dpid.spec.ts
@@ -1,16 +1,16 @@
-import DPiD from '../src/dpid';
+import DPid from '../src/dpid';
 
-const validMod36_37 = [
-	'5molwgwy88q4rvosx2',
-	'x3f92svglggaebo4y8',
-	'nc7a0kon7x1b9ebwa0'
+// Random strings with valid mod37_36 checksums
+const validMod37_36 = [
+	'k6br004z4iqg3hg2vw',
+	'x3f92svglggaebo4y2',
+	'nc7a0kon7x1b9ebwah'
 ];
 
-
-describe( 'validate() returns true', () => {
-	for( let i:number = validMod36_37.length - 1; i >= 0; i-- ) {
-		it( 'Should return true when input is ' + validMod36_37[ i ], () => {
-			expect( DPiD.validate( validMod36_37[ i ] ) ).toBe( true );
+describe( 'validate() returns true with valid mod37_36 checksums', () => {
+	for( let i:number = validMod37_36.length - 1; i >= 0; i-- ) {
+		it( 'Should return true when input is ' + validMod37_36[ i ], () => {
+			expect( DPid.validate( validMod37_36[ i ] ) ).toBe( true );
 		} );
 	}
 } );
