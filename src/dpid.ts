5569495bd86137662f8502de1825db2b4d5b258e
Laine Farrell <laine@openimp.com>
2021-03-17 12:56:37 +0000

    Adding basic dpid code, writing a test that polls the object and returns true

---
+++ src/dpid.ts
@@ -0,0 +1,9 @@
+import { mod37_36 } from 'cdigit';
+
+const DPiD = {
+	validate: function( inp:string ):boolean {
+		return true;
+	}
+};
+
+export default DPiD;
--- test/dpid.spec.ts
+++ test/dpid.spec.ts
@@ -1,2 +1,5 @@
+import DPiD from '../src/dpid';
+
 test( 'DPiD test', () => {
+	expect( DPiD.validate( '5molwgwy88q4rvosx2' ) ).toBe( true );
 } );
